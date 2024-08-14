f4419182c8a763775039851768bf856ef18212a9
Sanjaya Niroula <niroulasanjaya42@fmail.com>
2024-08-14 22:43:11 +0545

    still making

--- anim.js
+++ anim.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    gsap.from(".my-name",{
+    gsap.from(".niroula",{
         scale:0,
-        x:"-50%",
+        y:"100%",
         delay:1,
         duration:1.5, 
         
     })
-    gsap.from("nav",{
+    gsap.from(".sanjaya",{
+        scale:0,
         y:"-100%",
         delay:1,
         duration:1.5, 
@@ -19,9 +20,21 @@ document.addEventListener('DOMContentLoaded', () => {
         scrollTrigger:{
             trigger:"#heading",
             scroller: "body",
-            markers:true,
             start:"top 0%",
             end:"top -100%",
+            scrub:2,
+            pin:true
+
+
+        }
+    })
+    gsap.to("#second-heading",{
+        transform: "translateX(-100%)",
+        scrollTrigger:{
+            trigger:"#second-heading",
+            scroller: "body",
+            start:"top 0%",
+            end:"top -200%",
             scrub:2,
             pin:true
 
